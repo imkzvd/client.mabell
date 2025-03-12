b3a76ff6e9b2d3cb927f449f003550ebe1046f22
imkzvd <imkzvd@gmail.com>
2025-03-13 02:29:50 +0300

    feat: changed extenstion of the style entrypoint file
    Closes: #8

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/scss/main.scss'],
   vite: {
     plugins: [
       tailwindcss()
