8422a64c3a176de77399470564096cdd9be880c2
imkzvd <imkzvd@gmail.com>
2025-08-28 18:02:17 +0300

    feat: deleted tailwind
    Closes: #13

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/scss/main.scss'],
-  vite: {
-    plugins: [
-      tailwindcss()
-    ]
-  }
+  css: ['~/assets/scss/main.scss']
 })
