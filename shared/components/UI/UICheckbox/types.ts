26e58d804ef078899429536ecbec77a7c7a5e1c0
imkzvd <imkzvd@gmail.com>
2025-08-30 05:13:07 +0300

    feat: added "UICheckbox" component
    Refs: #18

---
+++ shared/components/UI/UICheckbox/types.ts
@@ -0,0 +1,12 @@
+export type UICheckboxProps = {
+  modelValue: boolean;
+  id?: string;
+  name?: string;
+  label?: string;
+  sublabel?: string;
+  isDisabled?: boolean;
+};
+
+export type UICheckboxEmits = {
+  (e: "update:modelValue", value: boolean): void;
+};
