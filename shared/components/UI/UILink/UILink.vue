9226e347aa9b8a09509d11f40c5adea100f3fa7f
imkzvd <imkzvd@gmail.com>
2025-09-01 23:52:00 +0300

    feat: added "UILink" component
    Refs: #18

---
+++ shared/components/UI/UILink/UILink.vue
@@ -0,0 +1,34 @@
+<template>
+  <NuxtLink :to="to" :class="cssClasses">
+    <slot name="default" />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { UILinkProps } from "~/shared/components/UI/UILink/types";
+
+const props = defineProps<UILinkProps>();
+
+const baseClass: string = "ui-link";
+const cssClasses = computed(() => ({
+  [baseClass]: true,
+  [`${baseClass}_underline`]: props.underline,
+}));
+</script>
+
+<style scoped lang="scss">
+.ui-link {
+  color: var(--ui-link-color, white);
+  font-size: var(--ui-link-font-size, 16px);
+
+  @include respond-to(xl) {
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &_underline {
+    text-decoration: underline;
+  }
+}
+</style>
