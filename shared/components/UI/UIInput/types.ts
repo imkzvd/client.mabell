c4f3d69968bfb0e2df95ff78acf9c09256a16c3f
imkzvd <imkzvd@gmail.com>
2025-09-01 17:28:37 +0300

    feat: added "UIInput" component
    Refs: #18

---
+++ shared/components/UI/UIInput/types.ts
@@ -0,0 +1,42 @@
+export enum UIInputTypes {
+  email = 'email',
+  number = 'number',
+  password = 'password',
+  tel = 'tel',
+  text = 'text',
+}
+
+
+export type UIInputType = keyof typeof UIInputTypes;
+
+export enum UIInputSizes {
+  sm = 'sm',
+  md = 'md',
+  lg = 'lg'
+}
+
+export type UIInputSize = keyof typeof UIInputSizes;
+
+export type UIInputProps = {
+  modelValue: string | number;
+  id?: string;
+  name?: string;
+  type?: UIInputType;
+  size?: UIInputSize;
+  placeholder?: string;
+  label?: string;
+  minLength?: number;
+  maxLength?: number;
+  isDisabled?: boolean;
+  isReadonly?: boolean;
+  isRequired?: boolean;
+  isClearable?: boolean;
+};
+
+export type UIInputEmits = {
+  (e: 'update:modelValue', value: string): void;
+  (e: 'input', value: string): void;
+  (e: 'change', value: string): void;
+  (e: 'focus'): void;
+  (e: 'blur'): void;
+};
