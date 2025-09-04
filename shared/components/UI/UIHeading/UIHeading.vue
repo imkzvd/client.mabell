1591418d8a2e68be8e6426edfbc7f1200b871ee2
imkzvd <imkzvd@gmail.com>
2025-09-04 04:50:14 +0300

    feat: added "UIHeading" component
    Refs: #20

---
+++ shared/components/UI/UIHeading/UIHeading.vue
@@ -0,0 +1,44 @@
+<template>
+  <component :is="'h' + level" :class="cssClasses">
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+import type { UIHeadingProps } from '~/shared/components/UI/UIHeading/types.ts';
+
+const props = withDefaults(defineProps<UIHeadingProps>(), {
+  level: 1,
+});
+
+const baseClass = 'ui-heading';
+const cssClasses = computed<Record<string, boolean>>(() => ({
+  [baseClass]: true,
+  [`${baseClass}_level_${props.level}`]: true,
+  [`${baseClass}_leading_none`]: props.leadingNone,
+}));
+</script>
+
+<style scoped lang="scss">
+.ui-heading {
+  font-family: var(--title-font, sans-serif);
+
+  &_level {
+    &_1 {
+      font-size: 32px;
+    }
+
+    &_2 {
+      font-size: 20px;
+    }
+
+    &_3 {
+      font-size: 16px;
+    }
+  }
+
+  &_leading_none {
+    line-height: 1;
+  }
+}
+</style>
