af2ae000008a080b2174da2ee768a575dea0df19
imkzvd <imkzvd@gmail.com>
2025-09-04 23:26:36 +0300

    feat: added "AlbumSlider" component
    Refs: #20

---
+++ modules/album/components/AlbumSlider/AlbumSlider.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="album-slider">
+    <AlbumSliderCard v-for="item of items" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { AlbumSliderProps } from "~/modules/album/components/AlbumSlider/types";
+
+defineProps<AlbumSliderProps>();
+</script>
+
+<style scoped lang="scss">
+.album-slider {
+  display: flex;
+  gap: 8px;
+  overflow-x: auto;
+
+  @include respond-to(xl) {
+    gap: 12px;
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+</style>
