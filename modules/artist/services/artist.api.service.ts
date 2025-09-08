9df286567e866fefb590cb5d399944e74b1ff111
imkzvd <imkzvd@gmail.com>
2025-09-08 19:41:28 +0300

    feat: added artist api service
    Refs: #20

---
+++ modules/artist/services/artist.api.service.ts
@@ -0,0 +1,60 @@
+import { apiService } from '~/shared/services/api.service';
+import type { AlbumRO, ArtistRO, SimplifiedAlbumRO, SimplifiedAlbumsRO, TracksRO } from '~/api/api.module';
+import { ApiError } from '#shared/errors/api-error';
+
+export class ArtistApiService {
+  async getArtistById(artistId: string): Promise<ArtistRO> {
+    const { data, ok, error } = await apiService.artists.getArtist(artistId);
+
+    if (!ok) {
+      throw new ApiError(error.message, error.statusCode);
+    }
+
+    return data;
+  }
+
+  async getTopTracksById(artistId: string, pagination?: Partial<{ offset: number, limit: number }>): Promise<TracksRO> {
+    const { data, ok, error } = await apiService.artists.getArtistTracks(artistId, pagination);
+
+    if (!ok) {
+      throw new ApiError(error.message, error.statusCode);
+    }
+
+    return data;
+  }
+
+  async getAlbumsById(artistId: string, pagination?: Partial<{
+    offset: number,
+    limit: number
+  }>): Promise<SimplifiedAlbumsRO> {
+    const { data, ok, error } = await apiService.artists.getArtistAlbums(artistId, pagination);
+
+    if (!ok) {
+      throw new ApiError(error.message, error.statusCode);
+    }
+
+    return data;
+  }
+
+  async getLatestAlbumById(artistId: string): Promise<AlbumRO | null> {
+    const { data, ok, error } = await apiService.artists.getLatestRelease(artistId);
+
+    if (!ok) {
+      throw new ApiError(error.message, error.statusCode);
+    }
+
+    return data;
+  }
+
+  async getSimilarArtistsById(artistId: string): Promise<ArtistRO[]> {
+    const { data, ok, error } = await apiService.artists.getSimilarArtists(artistId);
+
+    if (!ok) {
+      throw new ApiError(error.message, error.statusCode);
+    }
+
+    return data;
+  }
+}
+
+export const artistApiService = new ArtistApiService();
