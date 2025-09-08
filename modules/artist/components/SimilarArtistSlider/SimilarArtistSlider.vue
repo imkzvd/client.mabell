1eec617797ac009a68133da04c0b46f6b852bed7
imkzvd <imkzvd@gmail.com>
2025-09-08 19:38:01 +0300

    feat: added "SimilarArtistSlider" component
    Refs: #20

---
+++ modules/artist/components/SimilarArtistSlider/SimilarArtistSlider.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="artist-slider">
+    <SimilarArtistSliderItem v-for="item of items" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { SimilarArtistSliderProps } from "~/modules/artist/components/SimilarArtistSlider/types";
+
+defineProps<SimilarArtistSliderProps>();
+</script>
+
+<style scoped lang="scss">
+.artist-slider {
+  display: flex;
+  gap: 8px;
+  overflow-x: auto;
+
+  @include respond-to(xl) {
+    gap: 12px;
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+</style>
