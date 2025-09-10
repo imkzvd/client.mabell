04c1519ad95d12dc4c9810da7bdafb19ba6e0511
imkzvd <imkzvd@gmail.com>
2025-09-10 18:33:44 +0300

    feat: updated "ArtistLinks" component
    Refs: #22

--- modules/artist/components/ArtistLinks/ArtistLinks.vue
+++ modules/artist/components/ArtistLinks/ArtistLinks.vue
@@ -3,14 +3,17 @@
     <div v-for="(item, index) of items" :key="item.id" class="artist-links__item">
       <UILink
         appearance="secondary"
-        :hover-underline="false"
+        :hover-underline="hoverUnderline"
         :to="{ name: 'artist', params: { id: item.id } }"
         class="artist-links__link"
         @click.stop
       >
         {{ item.name }}
       </UILink>
-      <span v-if="index !== items.length - 1">, </span>
+      <span v-if="index !== items.length - 1" :class="separatorCssClass">
+        <NuxtIcon v-if="dotSeparator" name="i-ph-dot-outline-fill" />
+        <template v-else>,</template>
+      </span>
     </div>
   </div>
 </template>
@@ -18,17 +21,33 @@
 <script setup lang="ts">
 import type { ArtistLinksProps } from '~/modules/artist/components/ArtistLinks/types';
 
-defineProps<ArtistLinksProps>()
+const props = defineProps<ArtistLinksProps>();
+
+const baseClass = 'artist-links';
+const separatorCssClass = computed(() => ({
+  [`${baseClass}__separator`]: true,
+  [`${baseClass}__separator_dot`]: props.dotSeparator,
+}));
 </script>
 
 <style scoped lang="scss">
 .artist-links {
   font-size: var(--size, inherit);
-  display: flex;
 
   &__item {
+    display: inline-flex;
+    align-items: center;
+
     &:not(:last-child) {
       margin-right: 2px;
+    }
+  }
+
+  &__separator {
+    color: var(--ui-link-color);
+
+    &_dot {
+      display: inline-flex;
     }
   }
 }
--- modules/artist/components/ArtistLinks/types.ts
+++ modules/artist/components/ArtistLinks/types.ts
@@ -1,5 +1,8 @@
 import type { ArtistRO, SimplifiedArtistRO } from '~/api/api.module';
+import type { UILinkProps } from '#shared/components/UI/UILink/types';
 
 export type ArtistLinksProps = {
   items: (ArtistRO | SimplifiedArtistRO)[];
+  dotSeparator?: boolean;
+  hoverUnderline?: UILinkProps['hoverUnderline'];
 };
