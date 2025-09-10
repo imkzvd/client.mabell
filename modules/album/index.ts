6b7ca53cdc76eea13c4613d40d6ecf7c9c47e827
imkzvd <imkzvd@gmail.com>
2025-09-10 18:32:11 +0300

    feat: added entry file of album module
    Refs: #22

---
+++ modules/album/index.ts
@@ -0,0 +1,23 @@
+import { defineNuxtModule } from '@nuxt/kit'
+import { resolve, join } from 'pathe'
+import type { Nuxt } from '@nuxt/schema'
+
+export default defineNuxtModule({
+  name: 'album-module',
+  configKey: 'album-module',
+  setup (options: any, nuxt: Nuxt) {
+    nuxt.hook('components:dirs', (dirs) => {
+      dirs.push({
+        path: join(__dirname, 'components')
+      })
+    })
+
+    nuxt.hook('pages:extend', (pages) => {
+      pages.push({
+        name: 'album',
+        path: '/album/:id',
+        file: resolve(__dirname, './pages/[id].vue')
+      })
+    })
+  }
+})
