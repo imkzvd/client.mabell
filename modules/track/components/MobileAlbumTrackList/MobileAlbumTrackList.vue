7f6f140e98be77a868a50af0b0522aa2fc728f76
imkzvd <imkzvd@gmail.com>
2025-09-10 17:56:01 +0300

    feat: added "MobileAlbumTrackList" component
    Refs: #22

---
+++ modules/track/components/MobileAlbumTrackList/MobileAlbumTrackList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul :aria-label="ariaLabel" class="mobile-album-track-list">
+    <MobileAlbumTrackListItem
+      v-for="(item, index) of items"
+      :key="item.id"
+      :item="item"
+      :index="index"
+      :current-item-id="currentItemId"
+      :is-playing="isPlaying"
+      @click="onClick(item, index)"
+      @open-item-menu="onItemMenuOpen($event, item, index)"
+    />
+  </ul>
+</template>
+
+<script setup lang="ts">
+import type {
+  MobileAlbumTrackListProps,
+  MobileAlbumTrackListEmits,
+} from "~/modules/track/components/MobileAlbumTrackList/types";
+import type { TrackRO } from '~/api/api.module';
+
+const props = defineProps<MobileAlbumTrackListProps>();
+const emit = defineEmits<MobileAlbumTrackListEmits>();
+
+function onClick(item: TrackRO, index: number) {
+  emit('play-item', item, index);
+}
+
+function onItemMenuOpen(e: Event, item: TrackRO, index: number) {
+  alert(`Open menu for ${item.name} track`)
+}
+</script>
---
+++ modules/track/components/MobileAlbumTrackList/types.ts
@@ -0,0 +1,29 @@
+import type { TrackRO } from "~/api/api.module";
+
+export type MobileAlbumTrackListProps = {
+  items: TrackRO[];
+  currentItemId?: string;
+  isPlaying?: boolean;
+  ariaLabel?: string;
+};
+
+export type MobileAlbumTrackListEmits = {
+  (e: "play-item", item: TrackRO, index: number): void;
+  (e: "pause-item", item: TrackRO, index: number): void;
+  (e: "add-item", item: TrackRO, index: number): void;
+};
+
+export type MobileAlbumTrackListItemProps = {
+  item: TrackRO;
+  index: number;
+  selectedItemId?: string;
+  currentItemId?: string;
+  isPlaying?: boolean;
+};
+
+export type MobileAlbumTrackListItemEmits = {
+  (e: "play-item"): void;
+  (e: "pause-item"): void;
+  (e: "add-item"): void;
+  (e: "open-item-menu", evt: Event): void;
+};
