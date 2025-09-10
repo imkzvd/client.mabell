afcce0aa5fd36696920d6f4a774831daef76e7fc
imkzvd <imkzvd@gmail.com>
2025-09-10 18:31:29 +0300

    feat: added album api service
    Refs: #22

---
+++ modules/album/services/album.api.service.ts
@@ -0,0 +1,27 @@
+import { apiService } from '~/shared/services/api.service';
+import { ApiError } from '#shared/errors/api-error';
+import type { AlbumRO, TracksRO } from '~/api/api.module';
+
+export class AlbumApiService {
+  async getAlbumById(albumId: string): Promise<AlbumRO> {
+    const { data, ok, error } = await apiService.album.getAlbum(albumId);
+
+    if (!ok) {
+      throw new ApiError(error.message, error.statusCode);
+    }
+
+    return data;
+  }
+
+  async getAlbumTracksById(albumId: string): Promise<TracksRO> {
+    const { data, ok, error } = await apiService.album.getAlbumTracks(albumId);
+
+    if (!ok) {
+      throw new ApiError(error.message,  error.statusCode);
+    }
+
+    return data;
+  }
+}
+
+export const albumApiService = new AlbumApiService();
