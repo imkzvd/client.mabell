ff796829037842c73d89a4ba073742cf208fc571
imkzvd <imkzvd@gmail.com>
2025-09-10 18:29:19 +0300

    feat: added track api service in track module
    Refs: #22

---
+++ modules/track/services/track.api.service.ts
@@ -0,0 +1,26 @@
+import { apiService } from '~/shared/services/api.service';
+import type { TracksRO } from '~/api/api.module';
+
+export class TrackApiService {
+  async getTopTracksByArtistId(id: string, pagination: Partial<{ offset: number, limit: number }>): Promise<TracksRO> {
+    const { data, ok, error } = await apiService.artists.getArtistTracks(id, pagination);
+
+    if (!ok) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  }
+
+  async getArtistTracksByArtistId(id: string, pagination: Partial<{ offset: number, limit: number }>): Promise<TracksRO> {
+    const { data, ok, error } = await apiService.artists.getArtistTracks(id, pagination);
+
+    if (!ok) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  }
+}
+
+export const trackApiService = new TrackApiService();
