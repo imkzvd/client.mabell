8d96d069c0f5ec91d61ce60a8c2d4abe81cb0fda
imkzvd <imkzvd@gmail.com>
2025-09-10 12:37:23 +0300

    feat: added "AlbumHeader" component
    Refs: #22

---
+++ modules/album/components/AlbumHeader/AlbumHeader.spec.ts
@@ -0,0 +1,68 @@
+import { describe, expect, test } from "vitest";
+import { render } from "@testing-library/vue";
+import AlbumHead from "~/components/AlbumHeader/MobileAlbumHeader.vue";
+import NuxtLinkStub from "~/__tests__/stubs/nuxt-link.stub";
+import type { TAlbumHeadProps } from "~/components/AlbumHeader/types";
+import FakeAlbumRO from "~/__tests__/ros/fake-album.ro";
+
+const fakeAlbum = new FakeAlbumRO();
+
+function renderComponent() {
+  return render(AlbumHead, {
+    global: {
+      stubs: {
+        NuxtLink: NuxtLinkStub,
+      },
+    },
+    props: <TAlbumHeadProps>{
+      album: fakeAlbum,
+    },
+  });
+}
+
+describe("AlbumHead", () => {
+  describe("User:", () => {
+    test("it will render the cover", () => {
+      const { getByRole } = renderComponent();
+
+      getByRole<HTMLImageElement>("img", { name: fakeAlbum.name });
+    });
+
+    test("it will render artist's links", () => {
+      const { getByRole } = renderComponent();
+
+      fakeAlbum.artists.forEach(({ id, name }) => {
+        const artistLink = getByRole<HTMLAnchorElement>("link", { name });
+        expect(artistLink.href).toBe(`${window.location.origin}/artist/${id}`);
+      });
+    });
+
+    test("it will render the 'Album' type", () => {
+      const { getByText } = renderComponent();
+
+      getByText<HTMLSpanElement>("Album");
+    });
+
+    test("it will render the genres of the album", () => {
+      const { getByText } = renderComponent();
+      const albumGenres = fakeAlbum.genres.join("/");
+
+      getByText<HTMLSpanElement>(albumGenres);
+    });
+
+    test("it will render the release year of the album", () => {
+      const { getByText } = renderComponent();
+      const albumReleaseYear = new Date(fakeAlbum.releaseDate).getFullYear();
+
+      getByText<HTMLSpanElement>(albumReleaseYear);
+    });
+  });
+
+  describe("Developer:", () => {
+    test("snapshot", () => {
+      const { container } = renderComponent();
+
+      expect(container).toMatchSnapshot();
+    });
+  });
+});
---
+++ modules/album/components/AlbumHeader/AlbumHeader.vue
@@ -0,0 +1,103 @@
+<template>
+  <header class="album-header">
+    <div class="container">
+      <div class="album-header__columns">
+        <AlbumCover :url="album.cover" :alt="album.name" class="album-header__cover" />
+
+        <div class="album-header__details">
+          <UIText class="album-header__meta-info">
+            <span>{{ album.type.label }}</span> -
+            <span>{{ albumGenres }}</span> -
+            <span v-if="releaseAlbumYear">{{ releaseAlbumYear }}</span>
+          </UIText>
+
+          <UIHeading class="album-header__name">
+            {{ album.name }}
+          </UIHeading>
+
+          <ArtistLinks :items="album.artists" dot-separator class="album-header__artist-links" />
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import type { MobileAlbumHeaderProps } from '~/modules/album/components/MobileAlbumHeader/types';
+
+const props = defineProps<MobileAlbumHeaderProps>();
+
+const albumGenres = computed<string>(() =>
+  props.album.genres.map(({ label }) => label).join("/"),
+);
+const releaseAlbumYear = computed<number | null>(() => {
+  if (!props.album.releaseAt) return null;
+
+  return new Date(props.album.releaseAt).getFullYear();
+});
+</script>
+
+<style scoped lang="scss">
+.album-header {
+  --album-color: v-bind(props.album.color);
+  display: flex;
+  align-items: center;
+  height: 50vh;
+  padding-block: 16px;
+  background: linear-gradient(
+      0deg,
+      var(--base-bg) 0%,
+      var(--album-color, #121212) 80%
+  );
+
+  &__columns {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 32px;
+    height: 100%;
+    min-height: 400px;
+  }
+
+  &__cover {
+    --size: 200px;
+    box-shadow: 0 1px 10px 0 #000000;
+    flex-shrink: 0;
+
+    @include respond-to(xl) {
+      --size: 300px;
+    }
+  }
+
+  &__details {
+    text-align: center;
+
+    @include respond-to(md) {
+      text-align: left;
+    }
+  }
+
+  &__name {
+    margin-bottom: 4px;
+    line-height: 1.2;
+
+    @include respond-to(xl) {
+      margin-bottom: 8px;
+    }
+  }
+
+  &__artist-links {
+    --ui-link-color: var(--main-text, white);
+    --size: 16px;
+  }
+
+  &__meta-info {
+    font-size: 12px;
+    color: var(--secondary-text, gray);
+
+    @include respond-to(xs) {
+      font-size: 14px;
+    }
+  }
+}
+</style>
