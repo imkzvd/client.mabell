ea67526e08e6d5f0b5bebde814b38235ab2c9187
imkzvd <imkzvd@gmail.com>
2025-09-19 07:22:51 +0300

    feat: updated "ArtistNames" component
    Refs: #26

--- modules/artist/components/ArtistNames/types.ts
+++ modules/artist/components/ArtistNames/types.ts
@@ -1,5 +1,15 @@
 import type { ArtistRO, SimplifiedArtistRO } from "~/api/api.module";
+import type { UITextProps } from '#shared/components/UI/UIText/types';
+
+export enum ArtistNamesSeparators {
+  comma = 'comma',
+  dot = 'dot'
+}
+
+export type ArtistNamesSeparator = keyof typeof ArtistNamesSeparators;
 
 export type ArtistNamesProps = {
   items: (ArtistRO | SimplifiedArtistRO)[];
+  lineClamp?: UITextProps['lineClamp'];
+  separator?: ArtistNamesSeparator;
 };
