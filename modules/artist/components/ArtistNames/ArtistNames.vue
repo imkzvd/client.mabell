617c2c89f6918bac351211017c73099178b31e18
imkzvd <imkzvd@gmail.com>
2025-09-19 07:22:51 +0300

    feat: updated "ArtistNames" component
    Refs: #26

--- modules/artist/components/ArtistNames/ArtistNames.vue
+++ modules/artist/components/ArtistNames/ArtistNames.vue
@@ -1,25 +1,43 @@
 <template>
-  <span class="artist-names">
-    <template v-for="(item, index) of allArtistNames" :key="index">
-      <span>{{ item }}</span>
-
-      <span v-if="index !== items.length - 1" class="artist-names__item">,</span>
-    </template>
-  </span>
+  <UIText class="artist-names" appearance="secondary" :line-clamp="lineClamp">
+    <span v-for="(item, index) of allArtistNames" :key="index" class="artist-names__item" :class="itemCssClass">
+      {{ item }}
+    </span>
+  </UIText>
 </template>
 
 <script setup lang="ts">
-import type { ArtistNamesProps } from "~/modules/artist/components/ArtistNames/types";
+import { type ArtistNamesProps, ArtistNamesSeparators } from '~/modules/artist/components/ArtistNames/types';
 
-const props = defineProps<ArtistNamesProps>();
+const props = withDefaults(defineProps<ArtistNamesProps>(), {
+  separator: ArtistNamesSeparators.comma,
+});
 
+const baseClass = 'artist-names';
+const itemCssClass = computed(() => ({
+  [`${baseClass}__item_separator_${props.separator}`]: true,
+}));
 const allArtistNames = computed(() => props.items.map(({ name }) => name));
 </script>
 
 <style scoped lang="scss">
 .artist-names {
   &__item {
-    margin-right: 2px;
+    &_separator {
+      &_comma {
+        &:not(:last-child)::after {
+          content: ',';
+          margin-right: 2px;
+        }
+      }
+
+      &_dot {
+        &:not(:last-child)::after {
+          content: '•';
+          margin-inline: 2px;
+        }
+      }
+    }
   }
 }
 </style>
