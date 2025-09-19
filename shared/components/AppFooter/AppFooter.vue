61cd4095cef3823dffaf3e7485ed6f657f991078
imkzvd <imkzvd@gmail.com>
2025-09-19 07:11:10 +0300

    feat: added "AppFooter" component
    Refs: #26

---
+++ shared/components/AppFooter/AppFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="app-footer py-8">
+    <div class="container">
+      <div class="app-footer__copyright mb-2">Copyright © 2024</div>
+
+      <div class="app-footer__links">
+        <UILink class="mr-1">Privacy</UILink>&
+        <UILink>Feedback</UILink>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.app-footer {
+  padding-block: 32px;
+  font-size: 12px;
+  color: var(--secondary-text);
+
+  &__copyright {
+    margin-bottom: 8px;
+  }
+}
+</style>
