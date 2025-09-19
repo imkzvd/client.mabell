ca66eee293b789d8fb27cb4537046d082cb485b7
imkzvd <imkzvd@gmail.com>
2025-09-19 07:10:49 +0300

    feat: added "HomeLogoLink" component
    Refs: #26

---
+++ shared/components/HomeLogoLink/HomeLogoLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLink :to="{ name: 'index' }" class="home-logo-link">
+    <HomeLogo :width="width" :height="height" />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { HomeLogoLinkProps } from '@/shared/components/HomeLogoLink/types.ts';
+import HomeLogo from '#shared/components/HomeLogo/HomeLogo.vue';
+
+defineProps<HomeLogoLinkProps>();
+</script>
+
+<style lang="scss" scoped></style>
