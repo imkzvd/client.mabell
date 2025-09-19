944ce01c844a4679152f26961ec1cc17d52a1f54
imkzvd <imkzvd@gmail.com>
2025-09-19 07:28:33 +0300

    feat: added index file for track module
    Refs: #26

---
+++ modules/track/index.ts
@@ -0,0 +1,17 @@
+import { defineNuxtModule } from '@nuxt/kit'
+import { join } from 'pathe'
+import type { Nuxt } from '@nuxt/schema'
+
+export default defineNuxtModule({
+  name: 'track-module',
+  configKey: 'track-module',
+  setup (options: any, nuxt: Nuxt) {
+
+    // Auto register components
+    nuxt.hook('components:dirs', (dirs) => {
+      dirs.push({
+        path: join(__dirname, 'components')
+      })
+    })
+  }
+})
