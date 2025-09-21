7901429defc001d46b15a08b86f2e13cfbf991c4
imkzvd <imkzvd@gmail.com>
2025-09-21 08:23:35 +0300

    fix: fixed api url env error
    Closes: #30

--- modules/shared/services/api.service.ts
+++ modules/shared/services/api.service.ts
@@ -3,7 +3,7 @@ import { ApiModule } from '~/api/api.module';
 let isRefreshing = false;
 
 export const apiService = new ApiModule({
-  baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl: process.env.API_URL,
   baseApiParams: {
     credentials: 'include',
   }
