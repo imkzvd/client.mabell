574ef6a207e2c92f4947e4f0e30bd5cfdfb3baa4
imkzvd <imkzvd@gmail.com>
2025-09-25 14:30:51 +0300

    feat: added playlist api service
    Refs: #40

---
+++ modules/playlist/services/playlist.api.service.ts
@@ -0,0 +1,27 @@
+import { apiService } from '~/modules/shared/services/api.service';
+import { ApiError } from '~/modules/shared/errors/api-error';
+import type { PlaylistRO, PlaylistTracksRO } from '~/api/api.module';
+
+export class PlaylistApiService {
+  async getPlaylistById(playlistId: string): Promise<PlaylistRO> {
+    const { data, ok, error } = await apiService.playlist.getPlaylist(playlistId);
+
+    if (!ok) {
+      throw new ApiError(error.message, error.statusCode);
+    }
+
+    return data;
+  }
+
+  async getPlaylistTracksById(playlistId: string): Promise<PlaylistTracksRO> {
+    const { data, ok, error } = await apiService.playlist.getPlaylistTracks(playlistId);
+
+    if (!ok) {
+      throw new ApiError(error.message,  error.statusCode);
+    }
+
+    return data;
+  }
+}
+
+export const playlistApiService = new PlaylistApiService();
