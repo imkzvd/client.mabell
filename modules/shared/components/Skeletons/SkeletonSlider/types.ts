8e361f2ce25a0f3a9733547ad1024ffb2449ce61
imkzvd <imkzvd@gmail.com>
2025-10-13 01:16:38 +0300

    feat: added "SkeletonSlider" component
    Refs: #44

---
+++ modules/shared/components/Skeletons/SkeletonSlider/types.ts
@@ -0,0 +1,12 @@
+import type { UIAlignment } from '~/modules/shared/components/UI/types';
+
+export type SkeletonSliderProps = {
+  total?: number;
+  align?: UIAlignment;
+  isRounded?: boolean;
+}
+
+export type SkeletonSliderItemProps = {
+  align?: UIAlignment;
+  isRounded?: boolean;
+}
