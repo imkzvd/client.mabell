91a0efe04ddf97151a4c4cf11a216f34d360f25e
imkzvd <imkzvd@gmail.com>
2025-10-13 01:22:50 +0300

    feat: added "PlaylistSlider" component
    Refs: #44

---
+++ modules/playlist/components/PlaylistSlider/PlaylistSlider.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="playlist-slider">
+    <PlaylistSliderItem v-for="item of items" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PlaylistSliderProps } from '~/modules/playlist/components/PlaylistSlider/types';
+
+defineProps<PlaylistSliderProps>();
+</script>
+
+<style scoped lang="scss">
+.playlist-slider {
+  display: flex;
+  gap: var(--album-slider-column-gap, 12px);
+  overflow-x: auto;
+
+  @include hide-scrollbar();
+
+  @include respond-to(xs) {
+    gap: var(--album-slider-column-xs-gap, 16px);
+  }
+}
+</style>
---
+++ modules/playlist/components/PlaylistSlider/PlaylistSliderItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <UILink
+    :hover-underline="false"
+    :to="{ name: 'playlist-id', params: { id: item.id } }"
+    class="playlist-slider-item"
+  >
+    <UIImg :url="item.cover" :alt="item.name" class="playlist-slider-item__cover" />
+
+    <div class="playlist-slider-item__details">
+      <UIText :line-clamp="2" class="playlist-slider-item__name">
+        {{ item.name }}
+      </UIText>
+    </div>
+  </UILink>
+</template>
+
+<script setup lang="ts">
+import type { PlaylistSliderItemProps } from '~/modules/playlist/components/PlaylistSlider/types';
+
+defineProps<PlaylistSliderItemProps>();
+</script>
+
+<style scoped lang="scss">
+.playlist-slider-item {
+  width: min-content;
+
+  &__cover {
+    --size: var(--album-slider-cover-size, 120px);
+    margin-bottom: 8px;
+
+    @include respond-to(xs) {
+      --size: var(--album-slider-cover-xs-size, 140px);
+      margin-bottom: 12px;
+    }
+
+    @include respond-to(md) {
+      --size: var(--album-slider-cover-md-size, 160px);
+    }
+  }
+
+  &__name {
+    font-size: 14px;
+
+    @include respond-to(md) {
+      font-size: 16px;
+    }
+  }
+}
+</style>
