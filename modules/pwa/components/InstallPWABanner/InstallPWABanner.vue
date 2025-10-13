cc868b88ebd13f90b5f274db1de6b6ce788599e7
imkzvd <imkzvd@gmail.com>
2025-10-13 22:02:33 +0300

    feat: added pwa module
    Refs: #46

---
+++ modules/pwa/components/InstallPWABanner/InstallPWABanner.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="install-pwa-banner">
+    <div class="install-pwa-banner__columns">
+      <Logo width="32px" height="32px" class="install-pwa-banner__logo" />
+
+      <div class="install-pwa-banner__details">
+        <UIText is-bold class="install-pwa-banner__title">
+          Mabell Application
+        </UIText>
+
+        <UIText appearance="secondary" class="install-pwa-banner__subtitle">
+          Discover new music every day
+        </UIText>
+      </div>
+
+      <UIButton size="sm" @click="onClick">Get App</UIButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const router = useRouter();
+
+function onClick() {
+  router.push({ name: 'pwa' });
+}
+</script>
+
+<style scoped lang="scss">
+.install-pwa-banner {
+  &__columns {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    align-items: center;
+    column-gap: 8px;
+  }
+
+  &__column {
+    display: flex;
+  }
+
+  &__logo {
+    width: 32px;
+    height: 32px;
+
+    @include respond-to(xs) {
+      width: 40px;
+      height: 40px;
+    }
+  }
+
+  &__details {
+    line-height: 1.2;
+  }
+
+  &__title {
+    font-size: 14px;
+
+    @include respond-to(xs) {
+      font-size: 16px;
+    }
+  }
+
+  &__subtitle {
+    font-size: 12px;
+
+    @include respond-to(xs) {
+      font-size: 14px;
+    }
+  }
+}
+</style>
---
+++ modules/pwa/pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="pwa-page">
+    <div class="container">
+      <UIHeading class="pwa-page__heading">Mabell Application</UIHeading>
+    </div>
+
+    <UISection with-container>
+      <dl class="pwa-page__instruction-list">
+        <dt class="pwa-page__instruction-list-term">
+          1. Tap the Share Icon
+        </dt>
+
+        <dd class="pwa-page__instruction-list-definition">
+          <UIText size="14px" appearance="secondary" class="pwa-page__item-description">
+            Tap the Share icon <NuxtIcon name="i-material-symbols-light-ios-share" size="16" /> located at the bottom of the screen (the square with an upward arrow).
+          </UIText>
+
+          <NuxtImg class="pwa-page__image" src="/images/pwa-instruction-1.jpg" />
+        </dd>
+
+        <dt class="pwa-page__instruction-list-term">2. Add to Home Screen</dt>
+
+        <dd class="pwa-page__instruction-list-definition">
+          <UIText size="14px" appearance="secondary" class="pwa-page__item-description">
+            In the Share menu, scroll through the options and tap on Add to Home Screen.
+          </UIText>
+
+          <NuxtImg class="pwa-page__image" src="/images/pwa-instruction-2.jpg" />
+        </dd>
+
+        <dt class="pwa-page__instruction-list-term">3. Launch the Mabell App</dt>
+
+        <dd class="pwa-page__instruction-list-definition">
+          <UIText size="14px" appearance="secondary" class="pwa-page__item-description">
+            After installation, you’ll find the app icon on your Home Screen. Tap the icon to launch the Mabell App.
+          </UIText>
+
+          <NuxtImg class="pwa-page__image" src="/images/pwa-instruction-3.jpg" />
+        </dd>
+      </dl>
+    </UISection>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.pwa-page {
+  padding-block: 32px;
+
+  &__heading {
+    margin-bottom: 32px;
+  }
+
+  &__instruction-list-term {
+    margin-bottom: 8px;
+  }
+
+  &__instruction-list-definition {
+    &:not(:last-child) {
+      margin-bottom: 32px;
+    }
+  }
+
+  &__item-description {
+    margin-bottom: 16px;
+  }
+
+  &__image {
+    border-radius: 16px;
+    border: 1px solid var(--gray-darkest, #ccc);
+  }
+}
+</style>
