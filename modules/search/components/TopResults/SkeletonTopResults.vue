2e5646605abb6f6bd3b231ac16ba0ddea9fc9862
imkzvd <imkzvd@gmail.com>
2025-10-13 05:21:53 +0300

    feat: TopResults components
    Refs: #44

---
+++ modules/search/components/TopResults/SkeletonTopResults.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="skeleton-top-results">
+    <SkeletonTopResultsItem v-for="(item, index) of 10" :with-rounded-image="!!!index"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.skeleton-top-results {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 320px);
+  gap: 16px;
+
+  &__item {
+    background-color: var(--gray-darkest);
+    border-radius: var(--border-rounded, 4px);
+    padding: 16px;
+    height: 92px;
+    animation: pulse-bg 1s infinite;
+  }
+}
+</style>
