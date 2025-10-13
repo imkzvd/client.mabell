e99d4b19f037f667ef34c306cd040988d6b6444f
imkzvd <imkzvd@gmail.com>
2025-10-13 05:25:26 +0300

    gcmsg: added "GlobalSearch" component
    Refs: #44

---
+++ modules/search/components/GlobalSearch/GlobalSearch.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="global-search">
+    <UISection with-container>
+      <UIInput
+        size="lg"
+        placeholder="Enter the name of the artist, album, track, playlist"
+        class="global-search__input"
+        is-clearable
+        v-model="modelValue"
+        @update:model-value="onModelValueUpdate"
+        @blur="onBlur"
+      />
+    </UISection>
+
+    <template v-if="isSearchFetching">
+      <div class="container">
+        <SkeletonSection>
+          <SkeletonTopResults />
+        </SkeletonSection>
+
+        <SkeletonSection v-if="isDesktopOrTablet">
+          <SkeletonSlider />
+        </SkeletonSection>
+      </div>
+    </template>
+    <template v-else-if="searchResult">
+      <UISection v-if="searchResult?.topResults.length" heading="Top  Results" with-container>
+        <TopResults :items="searchResult.topResults.slice(0,108)" />
+      </UISection>
+
+      <UISection v-if="searchResult?.albums.length" heading="Albums" :content-container="false">
+        <AlbumSlider :items="searchResult?.albums" class="global-search__slider" />
+      </UISection>
+
+      <UISection v-if="searchResult?.artists.length" heading="Artists" :content-container="false">
+        <ArtistSlider :items="searchResult?.artists" class="global-search__slider" />
+      </UISection>
+
+      <UISection v-if="searchResult?.tracks.length" heading="Tracks" with-container>
+        <TopResults :items="searchResult.tracks" />
+      </UISection>
+
+      <UISection v-if="searchResult?.playlists.length" heading="Playlists" :content-container="false">
+        <PlaylistSlider :items="searchResult.playlists" />
+      </UISection>
+
+      <UIText
+        v-if="
+        !searchResult?.topResults.length &&
+        !searchResult?.albums.length &&
+        !searchResult?.artists.length &&
+        !searchResult?.tracks.length &&
+        !searchResult?.playlists.length"
+        size="16px"
+        is-bold
+        align="center"
+      >
+        No Results
+      </UIText>
+    </template>
+    <slot v-else name="default" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { searchApiService } from '~/modules/search/services/search.api-service';
+import type { SearchResultRO } from '~/api/api.module';
+
+const router = useRouter();
+const route = useRoute();
+const { isDesktopOrTablet } = useDevice();
+const queryFromRouter = route.query.q as string | undefined;
+const debounceSearch = useDebounceFn(search, 500);
+const [isSearchFetching, toggleSearchFetching] = useToggle();
+
+const modelValue = ref<string>(queryFromRouter || '');
+const searchResult = ref<SearchResultRO | null>(null);
+
+onMounted(() => {
+  if (modelValue.value) {
+    search(modelValue.value);
+  }
+});
+
+async function onModelValueUpdate(q: string | null) {
+  if (!q) {
+    searchResult.value = null;
+
+    return;
+  }
+
+  await debounceSearch(q);
+}
+
+function onBlur(value: string) {
+  router.push({ query: { q: value } });
+}
+
+async function search(q: string) {
+  try {
+    toggleSearchFetching();
+    searchResult.value = await searchApiService.search(q);
+  } catch (e) {
+    const { message } = e as Error;
+
+    console.error(message);
+  } finally {
+    toggleSearchFetching();
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@use '~/assets/scss/container';
+
+.global-search {
+  &__input {
+    margin-bottom: 16px;
+  }
+
+  &__slider {
+    @extend .container;
+  }
+}
+</style>
