ab764d1b0922522efa0f0ed4d79fca045b51eb52
imkzvd <imkzvd@gmail.com>
2025-10-28 17:56:12 +0300

    feat: added "SkeletonTextLoader" component
    Refs: #63

---
+++ modules/shared/components/Skeletons/SkeletonTextLoader/SkeletonTextLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="skeleton-text-loader" />
+</template>
+
+<script setup lang="ts">
+import type { SkeletonTextLoaderProps } from '~/modules/shared/components/Skeletons/SkeletonTextLoader/types';
+
+withDefaults(defineProps<SkeletonTextLoaderProps>(), {
+  width: '70%',
+});
+</script>
+
+<style scoped lang="scss">
+.skeleton-text-loader {
+  width: v-bind(width);
+  height: 16px;
+  border-radius: var(--border-radius, 4px);
+  animation: pulse-bg 1s infinite;
+}
+</style>
---
+++ modules/shared/components/Skeletons/SkeletonTextLoader/types.ts
@@ -0,0 +1,3 @@
+export type SkeletonTextLoaderProps = {
+  width?: string;
+};
