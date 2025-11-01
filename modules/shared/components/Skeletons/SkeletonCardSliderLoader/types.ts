ee7b2a1214a4f78be0c67d78514557b402fe2c12
imkzvd <imkzvd@gmail.com>
2025-11-01 07:29:33 +0300

    feat: added "SkeletonCardSliderLoader" component
    Refs: #63

---
+++ modules/shared/components/Skeletons/SkeletonCardSliderLoader/types.ts
@@ -0,0 +1,7 @@
+import type { UIAlignment } from '~/modules/shared/components/UI/types';
+
+export type SkeletonCardSliderLoaderProps = {
+  textRows?: '1' | '2';
+  roundedImage?: boolean;
+  align?: UIAlignment;
+};
