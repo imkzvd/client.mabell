846ec3ae7ae04da894cf54bb7756f185b4d1dd1c
imkzvd <imkzvd@gmail.com>
2025-11-04 04:39:49 +0300

    feat: added "SkeletonSectionLoader" component
    Closes: #63

---
+++ modules/shared/components/Skeletons/SkeletonSectionLoader/SkeletonSectionLoader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="skeleton-section-loader">
+    <SkeletonHeadingLoader class="skeleton-section-loader__heading" />
+
+    <slot name="default" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.skeleton-section-loader {
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+
+  @include respond-to(md) {
+    &:not(:last-child) {
+      margin-bottom: 32px;
+    }
+  }
+
+  &__heading {
+    margin-bottom: 16px;
+  }
+}
+</style>
