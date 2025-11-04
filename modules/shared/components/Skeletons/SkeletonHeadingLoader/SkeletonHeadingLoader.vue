2896f26273f5e642b69241938002dad806a03761
imkzvd <imkzvd@gmail.com>
2025-11-04 04:39:12 +0300

    feat: added "SkeletonHeadingLoader" component
    Refs: #63

---
+++ modules/shared/components/Skeletons/SkeletonHeadingLoader/SkeletonHeadingLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="skeleton-heading-loader" />
+</template>
+
+<script setup lang="ts">
+import type { SkeletonHeadingLoaderProps } from '~/modules/shared/components/Skeletons/SkeletonHeadingLoader/types';
+
+withDefaults(defineProps<SkeletonHeadingLoaderProps>(), {
+  width: '50%',
+});
+</script>
+
+<style scoped lang="scss">
+.skeleton-heading-loader {
+  width: v-bind(width);
+  height: 32px;
+  border-radius: var(--border-radius, 4px);
+  animation: pulse-bg 1s infinite;
+}
+</style>
