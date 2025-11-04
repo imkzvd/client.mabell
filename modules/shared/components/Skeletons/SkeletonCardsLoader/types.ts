08f2bbeb664228591c46e6c838049d19d464fd70
imkzvd <imkzvd@gmail.com>
2025-11-04 04:32:17 +0300

    feat: updated "SkeletonCardsLoader" component and renamed to "SkeletonCardListLoader"
    Refs: #63

--- modules/shared/components/Skeletons/SkeletonCardsLoader/types.ts
+++
@@ -1,8 +0,0 @@
-import type { UIAlignment } from '~/modules/shared/components/UI/types';
-
-export type SkeletonCardsLoaderProps = {
-  total?: number;
-  rows?: '1' | '2';
-  roundedImage?: boolean;
-  align?: UIAlignment;
-};
