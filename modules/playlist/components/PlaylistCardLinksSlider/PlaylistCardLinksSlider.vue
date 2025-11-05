5777c4d2d46acea3f8bcc78dad7ebcbd898465e9
imkzvd <imkzvd@gmail.com>
2025-11-05 07:00:05 +0300

    feat: added "PlaylistCardLinksSlider" component
    Refs: #65

---
+++ modules/playlist/components/PlaylistCardLinksSlider/PlaylistCardLinksSlider.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="playlist-card-links-slider">
+    <PlaylistCardLinkSlider v-for="item of items" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PlaylistCardLinksSliderProps } from '~/modules/playlist/components/PlaylistCardLinksSlider/types';
+
+defineProps<PlaylistCardLinksSliderProps>();
+</script>
+
+<style scoped lang="scss">
+.playlist-card-links-slider {
+  display: flex;
+  column-gap: var(--card-links-gap, 12px);
+  overflow-x: auto;
+
+  @include respond-to(lg) {
+    column-gap: var(--card-links-lg-gap, 16px);
+  }
+}
+</style>
---
+++ modules/playlist/components/PlaylistCardLinksSlider/types.ts
@@ -0,0 +1,9 @@
+import type { PlaylistRO } from '~/api/api.module';
+
+export type PlaylistCardLinksSliderProps = {
+  items: PlaylistRO[];
+};
+
+export type PlaylistCardLinkSliderProps = {
+  item: PlaylistRO;
+};
