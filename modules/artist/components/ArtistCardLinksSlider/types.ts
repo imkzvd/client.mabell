b3254eaea75f822ad1bfbcfb3ab08b6f8cf70b1d
imkzvd <imkzvd@gmail.com>
2025-11-05 06:33:37 +0300

    feat: added "ArtistCardLinksSlider" component
    Refs: #65

---
+++ modules/artist/components/ArtistCardLinksSlider/types.ts
@@ -0,0 +1,9 @@
+import type { ArtistRO } from '~/api/api.module';
+
+export type ArtistCardLinksSliderProps = {
+  items: ArtistRO[];
+};
+
+export type ArtistCardLinkSliderProps = {
+  item: ArtistRO;
+};
