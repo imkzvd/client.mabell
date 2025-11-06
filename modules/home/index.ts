2e6e117bfc991e10b3f091e8812c9c24786ef0e8
imkzvd <imkzvd@gmail.com>
2025-11-06 07:15:19 +0300

    feat: added "home" module
    Refs: #69

---
+++ modules/home/index.ts
@@ -0,0 +1,26 @@
+import { defineNuxtModule, createResolver, extendPages, addComponentsDir } from '@nuxt/kit';
+import { join } from 'pathe';
+import type { Nuxt } from '@nuxt/schema';
+
+export default defineNuxtModule({
+  meta: {
+    name: 'home-module',
+    configKey: 'home-module',
+  },
+  setup(options: any, nuxt: Nuxt) {
+    const { resolve } = createResolver(import.meta.url);
+
+    addComponentsDir({
+      path: join(__dirname, 'components'),
+      pathPrefix: false,
+    });
+
+    extendPages((pages) => {
+      pages.push({
+        name: 'index',
+        path: '/',
+        file: resolve(__dirname, './pages/index.vue'),
+      });
+    });
+  },
+});
