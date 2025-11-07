45da5a6d87f9116231463259277a6eb0bf8bb64c
imkzvd <imkzvd@gmail.com>
2025-11-07 14:23:08 +0300

    feat: update home page component
    Refs: #69

---
+++ modules/home/constants.ts
@@ -0,0 +1,43 @@
+export const trendingMusicIds: string[] = [
+  '68e3e34af46edf0494ad78f1', // Sexyy Red, Bruno Mars || Fat Juicy & Wet
+  '68e41603f46edf0494ad7aa9', // J. Cole || MIDDLE CHILD
+  '68e33f46f46edf0494ad7721', // The Weeknd, Anitta || São Paulo
+  '68e418caf46edf0494ad7b12', // Young Thug, J. Cole, Travis Scott || The London
+  '68e34454f46edf0494ad779c', // Anitta || Envolver
+  '68e33529f46edf0494ad72fc', // Post Malone || Chemical
+  '68e32229f46edf0494ad6e28', // Chief Keef || Shake Dat
+  '68e324b7f46edf0494ad6eec', // Bad Bunny || L CLúB
+];
+
+export const topAlbumIds: string[] = [
+  '68e339d8f46edf0494ad74ed', // The Weeknd || Starboy
+  '68af396e082e45faa707e61c', // Drake || For All The Dogs
+  '68af2d76082e45faa707e03b', // PARTYNEXTDOOR, Drake || $ome $exy $ongs 4 U
+  '68e2b442f46edf0494ad6a1d', // Quavo || QUAVO HUNCHO
+  '68e32d2ef46edf0494ad7081', // Post Malone || beerbongs & bentleys
+  '68e4067ef46edf0494ad7a03', // Lil Yachty || The Secret Recipe
+  '68e41b9bf46edf0494ad7bb2', // Travis Scott || ASTROWORLD
+  '68e3267ef46edf0494ad6f54', // Taylor Swift || I Can Do It With a Broken Heart
+];
+
+export const hotPlaylistIds: string[] = [
+  '68df37d1f46edf0494ad620f', // Top Pop Hits
+  '68dfe92bf46edf0494ad6448', // ’90s Hip-Hop Essentials
+  '68dfe2d9f46edf0494ad62f3', // Rhythms of the City
+  '68dfe443f46edf0494ad6382', // Street Vibes & Beats
+  '68df2843f46edf0494ad610c', // New school.
+  '68d1565a7707b06ce780fdf2', // Hip-Hop Hits
+  '68dfdf18f46edf0494ad6265', // Pop Dreams
+  '68dfe348f46edf0494ad6338', // Feel-Good Pop Waves
+];
+
+export const popularArtistIds: string[] = [
+  '68af3d4c082e45faa707eb7f', // Travis Scott
+  '68e2b554f46edf0494ad6ae9', // Saweetie
+  '68af4346082e45faa707ef4c', // The Weeknd
+  '68e33bbbf46edf0494ad75c3', // Lana Del Rey
+  '68e33171f46edf0494ad71a6', // G-Eazy
+  '68af42d8082e45faa707ef3a', // Taylor Swift
+  '68e3e517f46edf0494ad7932', // Central Cee
+  '68e33fb7f46edf0494ad7729', // Anitta
+];
--- modules/home/pages/index.vue
+++ modules/home/pages/index.vue
@@ -1,9 +1,98 @@
 <template>
   <div class="home-page">
-    <UIHeading>Mabell</UIHeading>
+    <UISection heading="Trending Music" :with-container="!isMobileOrTablet">
+      <template v-if="isMobileOrTablet">
+        <SkeletonCardSliderLoader v-if="isFetching" text-rows="2" class="home-page__slider" />
+        <AlbumCardLinksSlider v-else :items="fetchedTrendingAlbums" class="home-page__slider" />
+      </template>
+      <template v-else>
+        <SkeletonCardListLoader v-if="isFetching" text-rows="2" />
+        <AlbumCardLinks v-else :items="fetchedTrendingAlbums" max-rows="1" />
+      </template>
+    </UISection>
+
+    <UISection heading="Top Albums" :with-container="!isMobileOrTablet">
+      <template v-if="isMobileOrTablet">
+        <SkeletonCardSliderLoader v-if="isFetching" text-rows="2" class="home-page__slider" />
+        <AlbumCardLinksSlider v-else :items="fetchedTopAlbums" class="home-page__slider" />
+      </template>
+      <template v-else>
+        <SkeletonCardListLoader v-if="isFetching" text-rows="2" />
+        <AlbumCardLinks v-else :items="fetchedTopAlbums" max-rows="1" />
+      </template>
+    </UISection>
+
+    <UISection heading="Hot Playlists" :with-container="!isMobileOrTablet">
+      <template v-if="isMobileOrTablet">
+        <SkeletonCardSliderLoader v-if="isFetching" text-rows="2" class="home-page__slider" />
+        <PlaylistCardLinksSlider v-else :items="fetchedHotPlaylists" class="home-page__slider" />
+      </template>
+      <template v-else>
+        <SkeletonCardListLoader v-if="isFetching" />
+        <PlaylistCardLinks v-else :items="fetchedHotPlaylists" max-rows="1" />
+      </template>
+    </UISection>
+
+    <UISection heading="Popular artists" :with-container="!isMobileOrTablet">
+      <template v-if="isMobileOrTablet">
+        <SkeletonCardSliderLoader v-if="isFetching" text-rows="2" class="home-page__slider" />
+        <ArtistCardLinksSlider v-else :items="fetchedPopularArtists" class="home-page__slider" />
+      </template>
+      <template v-else>
+        <SkeletonCardListLoader v-if="isFetching" is-rounded-image align="center" />
+        <ArtistCardLinks v-else :items="fetchedPopularArtists" max-rows="1" />
+      </template>
+    </UISection>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { AlbumRO, ArtistRO, PlaylistRO } from '~/api/api.module';
+import { albumApiService } from '~/modules/album/services/album.api.service';
+import { playlistApiService } from '~/modules/playlist/services/playlist.api.service';
+import { artistApiService } from '~/modules/artist/services/artist.api.service';
+import {
+  hotPlaylistIds,
+  popularArtistIds,
+  topAlbumIds,
+  trendingMusicIds,
+} from '~/modules/home/constants';
 
-<style scoped lang="scss"></style>
+const { isMobileOrTablet } = useDevice();
+const [isFetching, toggleFetching] = useToggle(true);
+
+const fetchedTrendingAlbums = ref<AlbumRO[]>([]);
+const fetchedTopAlbums = ref<AlbumRO[]>([]);
+const fetchedHotPlaylists = ref<PlaylistRO[]>([]);
+const fetchedPopularArtists = ref<ArtistRO[]>([]);
+
+onMounted(async () => {
+  const [trendingAlbums, topAlbums, hotPlaylists, popularArtists] = await Promise.all([
+    albumApiService.getAlbumsByIds(trendingMusicIds),
+    albumApiService.getAlbumsByIds(topAlbumIds),
+    playlistApiService.getPlaylistsByIds(hotPlaylistIds),
+    artistApiService.getArtistsByIds(popularArtistIds),
+  ]);
+
+  fetchedTrendingAlbums.value = trendingAlbums;
+  fetchedTopAlbums.value = topAlbums;
+  fetchedHotPlaylists.value = hotPlaylists;
+  fetchedPopularArtists.value = popularArtists;
+
+  toggleFetching();
+});
+</script>
+
+<style scoped lang="scss">
+@use '~/assets/scss/container';
+
+.home-page {
+  display: flex;
+  flex-direction: column;
+  padding-block: 32px;
+
+  &__slider {
+    @extend .container;
+  }
+}
+</style>
