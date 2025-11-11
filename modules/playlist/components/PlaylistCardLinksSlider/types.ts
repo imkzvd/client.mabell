bcdbaa0baf8571660eb5a803c951c8545c902f5f
imkzvd <imkzvd@gmail.com>
2025-11-11 06:39:03 +0300

    feat: updated "PlaylistCardLinksSlider" component
    Refs: #71

--- modules/playlist/components/PlaylistCardLinksSlider/types.ts
+++ modules/playlist/components/PlaylistCardLinksSlider/types.ts
@@ -3,7 +3,3 @@ import type { PlaylistRO } from '~/api/api.module';
 export type PlaylistCardLinksSliderProps = {
   items: PlaylistRO[];
 };
-
-export type PlaylistCardLinkSliderProps = {
-  item: PlaylistRO;
-};
