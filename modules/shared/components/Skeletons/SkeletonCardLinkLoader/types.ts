5f20a11aea9a51dfddd6725d826428d4097573a0
imkzvd <imkzvd@gmail.com>
2025-11-11 05:18:08 +0300

    feat: added "SkeletonCardLinkLoader" component
    Refs: #71

---
+++ modules/shared/components/Skeletons/SkeletonCardLinkLoader/types.ts
@@ -0,0 +1,7 @@
+import type { UIAlignment } from '~/modules/shared/components/UI/types';
+
+export type SkeletonCardLinkLoaderProps = {
+  textRows?: '1' | '2';
+  imageRounded?: boolean;
+  textAlign?: UIAlignment;
+};
