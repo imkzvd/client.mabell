051eefd31b9a7d959bb4221208152c1455717ebd
imkzvd <imkzvd@gmail.com>
2025-11-11 13:19:21 +0300

    feat: deleted old components
    Refs: #71

--- modules/shared/components/Skeletons/SkeletonCardListLoader/types.ts
+++
@@ -1,7 +0,0 @@
-import type { UIAlignment } from '~/modules/shared/components/UI/types';
-
-export type SkeletonCardListLoaderProps = {
-  textRows?: '1' | '2';
-  isRoundedImage?: boolean;
-  align?: UIAlignment;
-};
