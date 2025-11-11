9ecd72df834ae3daf1d13c17e5117bfe1868838c
imkzvd <imkzvd@gmail.com>
2025-11-11 06:37:47 +0300

    feat: added "PlaylistCardLink" component
    Refs: #71

---
+++ modules/playlist/components/PlaylistCardLink/PlaylistCardLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <CardLink
+    :to="{ name: 'playlist-id', params: { id: item.id } }"
+    :img-path="item.cover"
+    :img-alt="item.name"
+    class="playlist-card-link"
+  >
+    <template #title>{{ item.name }}</template>
+  </CardLink>
+</template>
+
+<script setup lang="ts">
+import type { PlaylistCardLinkProps } from '~/modules/playlist/components/PlaylistCardLink/types';
+
+defineProps<PlaylistCardLinkProps>();
+</script>
+
+<style scoped lang="scss"></style>
