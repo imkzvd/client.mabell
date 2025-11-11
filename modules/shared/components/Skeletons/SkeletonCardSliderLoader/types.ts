ae7233dd4d54e02abdf6b25033b187f944847794
imkzvd <imkzvd@gmail.com>
2025-11-11 05:33:17 +0300

    feat: deleted "SkeletonCardSliderLoade" component
    Refs: #71

--- modules/shared/components/Skeletons/SkeletonCardSliderLoader/types.ts
+++
@@ -1,7 +0,0 @@
-import type { UIAlignment } from '~/modules/shared/components/UI/types';
-
-export type SkeletonCardSliderLoaderProps = {
-  textRows?: '1' | '2';
-  isRoundedImage?: boolean;
-  align?: UIAlignment;
-};
