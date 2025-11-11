753c1c6dde851d57a5fc0b0de8f18c0279d51f9f
imkzvd <imkzvd@gmail.com>
2025-11-11 06:30:42 +0300

    feat: added "ArtistCardLink" component
    Refs: #71

---
+++ modules/artist/components/ArtistCardLink/ArtistCardLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <CardLink
+    :to="{ name: 'artist-id', params: { id: item.id } }"
+    :img-path="item.avatar"
+    :img-alt="item.name"
+    text-align="center"
+    img-rounded
+    class="artist-card-link"
+  >
+    <template #title>{{ item.name }}</template>
+  </CardLink>
+</template>
+
+<script setup lang="ts">
+import type { ArtistCardLinkProps } from '~/modules/artist/components/ArtistCardLink/types';
+
+defineProps<ArtistCardLinkProps>();
+</script>
+
+<style scoped lang="scss"></style>
