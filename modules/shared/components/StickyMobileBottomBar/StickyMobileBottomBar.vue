c28f0d6a230720af452d7e4b1ebcfa09916c98c2
imkzvd <imkzvd@gmail.com>
2025-11-12 10:22:51 +0300

    feat: updated "StickyMobileBottomBar" component
    Refs: #73

--- modules/shared/components/StickyMobileBottomBar/StickyMobileBottomBar.vue
+++ modules/shared/components/StickyMobileBottomBar/StickyMobileBottomBar.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="sticky-mobile-bottom-bar">
-    <MobileNavMenu />
+    <ClientOnly>
+      <MobilePlayer class="sticky-mobile-bottom-bar__mobile-player" />
+    </ClientOnly>
+
+    <MobileNavMenu class="sticky-mobile-bottom-bar__mobile-nav-menu" />
   </div>
 </template>
 
@@ -14,10 +18,18 @@
   right: 0;
   bottom: 0;
   left: 0;
+  padding: 4px 8px;
   padding-bottom: calc(env(safe-area-inset-bottom, 0));
-  border-top: 1px solid var(--nav-devider-color, gray);
-  background-color: rgba(18, 18, 18, 90%);
-  backdrop-filter: blur(15px) saturate(200%);
+  background-color: rgba(18, 18, 18, 95%);
+  border-top: 1px solid hsla(0, 0%, 50%, 0.1);
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  backdrop-filter: blur(30px) saturate(600%);
+  -webkit-backdrop-filter: blur(10px) saturate(200%);
   box-shadow: 0 -5px 10px rgba(0, 0, 0, 0.5);
+
+  &__mobile-player {
+    margin-block: 4px;
+  }
 }
 </style>
