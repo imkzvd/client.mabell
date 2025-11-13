3efb840080266ec8c9c6e9554a6549463ef8f9ab
imkzvd <imkzvd@gmail.com>
2025-11-13 19:18:33 +0300

    feat: added "noPadding" prop for "UISection" component
    Refs: #73

--- modules/shared/components/UI/UISection/UISection.vue
+++ modules/shared/components/UI/UISection/UISection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="ui-section">
+  <section class="ui-section" :class="rootCSSClasses">
     <template v-if="$slots.heading || heading">
       <div :class="headingContainer ? 'container' : 'container-fluid'">
         <slot name="heading">
@@ -19,15 +19,24 @@
 <script setup lang="ts">
 import type { UISectionProps } from '~/modules/shared/components/UI/UISection/types';
 
-withDefaults(defineProps<UISectionProps>(), {
+const props = withDefaults(defineProps<UISectionProps>(), {
   headingContainer: false,
   contentContainer: false,
 });
+
+const rootCSSClass = 'ui-section';
+const rootCSSClasses = computed<Record<string, boolean>>(() => ({
+  [`${rootCSSClass}_no-padding`]: props.noPadding,
+}));
 </script>
 
 <style scoped lang="scss">
 .ui-section {
   padding-block: var(--section-y-padding, 12px);
+
+  &_no-padding {
+    padding-block: 0;
+  }
 
   &__heading {
     margin-bottom: var(--section-heading-bottom-margin, 16px);
