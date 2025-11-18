142e6452ddc3439e5027ee83ab1ebfce1e283c91
imkzvd <imkzvd@gmail.com>
2025-11-18 14:40:38 +0300

    feat: updated "UISlider" component
    Refs: #75

--- modules/shared/components/UI/UISlider/types.ts
+++ modules/shared/components/UI/UISlider/types.ts
@@ -1,11 +1,12 @@
 export type UISliderProps = {
   modelValue: number;
   max?: number;
+  width?: string;
 };
 
 export type UISliderEmits = {
   (e: 'update:modelValue', value: number): void;
   (e: 'change', value: number): void;
-  (e: 'mouseup'): void;
-  (e: 'mousedown'): void;
+  (e: 'mouseup', evt: MouseEvent): void;
+  (e: 'mousedown', evt: MouseEvent): void;
 };
