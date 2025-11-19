ab7117cf9a942ef978810a60f182efc1c24236c0
imkzvd <imkzvd@gmail.com>
2025-11-19 08:58:15 +0300

    test: updated "UIForm" specs
    Refs: #75

--- modules/shared/components/UI/UIForm/UIForm.spec.ts
+++ modules/shared/components/UI/UIForm/UIForm.spec.ts
@@ -3,24 +3,41 @@ import { describe, expect, test } from 'vitest';
 import { userEvent } from '@testing-library/user-event';
 import UIForm from '~/modules/shared/components/UI/UIForm/UIForm.vue';
 import UIButton from '~/modules/shared/components/UI/UIButton/UIButton.vue';
+import UIInput from '~/modules/shared/components/UI/UIInput/UIInput.vue';
 import type { UIFormProps } from '~/modules/shared/components/UI/UIForm/types';
 import type { UIButtonProps } from '~/modules/shared/components/UI/UIButton/types';
+import type { UIInputProps } from '~/modules/shared/components/UI/UIInput/types';
 
 function renderComponent(props?: Partial<UIFormProps>) {
   return render(UIForm, {
-    props: { state: {} },
+    props: {
+      state: {},
+      ...props,
+    },
     slots: {
-      default: () => h(UIButton, <UIButtonProps>{ type: 'submit' }, { default: () => 'Submit' }),
+      default: () => [h(UIInput, <UIInputProps>{}), h(UIButton, <UIButtonProps>{ type: 'submit' })],
     },
   });
 }
 
 describe('UIForm', () => {
   describe('Default:', () => {
+    test('it will render the input', () => {
+      const { getByRole } = renderComponent();
+
+      getByRole<HTMLInputElement>('textbox');
+    });
+
     test('it will render submit button', () => {
       const { getByRole } = renderComponent();
 
-      getByRole<HTMLButtonElement>('button', { name: 'Submit' });
+      getByRole<HTMLButtonElement>('button');
+    });
+
+    test('snapshot', () => {
+      const { container } = renderComponent();
+
+      expect(container).toMatchSnapshot();
     });
   });
 
@@ -28,28 +45,11 @@ describe('UIForm', () => {
     test('it will emit "submit", if user click submit', async () => {
       const { getByRole, emitted } = renderComponent();
       const user = userEvent.setup();
-      const buttonEl = getByRole<HTMLButtonElement>('button', { name: 'Submit' });
+      const buttonEl = getByRole<HTMLButtonElement>('button');
 
       await user.click(buttonEl);
 
       expect(emitted('submit')).toBeTruthy();
     });
-
-    test('it will emit "submit", if user press enter', async () => {
-      const { getByRole, emitted } = renderComponent();
-      const user = userEvent.setup();
-      const formEl = getByRole<HTMLFormElement>('form');
-
-      await user.tab();
-      await user.keyboard('[Enter]');
-
-      expect(emitted('submit')).toBeTruthy();
-    });
-  });
-
-  test('snapshot', () => {
-    const { container } = renderComponent();
-
-    expect(container).toMatchSnapshot();
   });
 });
