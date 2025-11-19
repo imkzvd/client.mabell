707c8717189160beb87dfa22188a4e754206889d
imkzvd <imkzvd@gmail.com>
2025-11-19 08:24:58 +0300

    feat: updated "UIButton" component
    Refs: #75

--- modules/shared/components/UI/UIButton/types.ts
+++ modules/shared/components/UI/UIButton/types.ts
@@ -1,3 +1,5 @@
+import type { UIAppearance, UISize } from '~/modules/shared/components/UI/types';
+
 export enum UIButtonTypes {
   button = 'button',
   submit = 'submit',
@@ -5,25 +7,10 @@ export enum UIButtonTypes {
 
 export type UIButtonType = keyof typeof UIButtonTypes;
 
-export enum UIButtonSizes {
-  sm = 'sm',
-  md = 'md',
-  lg = 'lg',
-}
-
-export type UIButtonSize = keyof typeof UIButtonSizes;
-
-export enum UIButtonAppearances {
-  primary = 'primary',
-  secondary = 'secondary',
-}
-
-export type UIButtonAppearance = keyof typeof UIButtonAppearances;
-
 export type UIButtonProps = {
   type?: UIButtonType;
-  size?: UIButtonSize;
-  appearance?: UIButtonAppearance;
+  size?: UISize;
+  appearance?: UIAppearance;
   isFull?: boolean;
   isDisabled?: boolean;
   isLoading?: boolean;
