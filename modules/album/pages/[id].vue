d0f41157e8fa776e88e5f665ac25fcdc637c1b54
imkzvd <imkzvd@gmail.com>
2025-11-19 17:33:02 +0300

    feat: updated album page component
    Closes: #89

--- modules/album/pages/[id].vue
+++ modules/album/pages/[id].vue
@@ -5,54 +5,61 @@
       <AlbumHeader v-else :album="fetchedAlbum" />
     </template>
 
-    <UISection content-container>
-      <div v-if="fetchedAlbumTracks?.total" class="album-page__top-tracks">
-        <MobileTrackList
-          v-if="isMobile"
-          :items="fetchedAlbumTracks?.items"
-          :is-playing="$audioPlayer?.isPlaying.value"
-          :current-item-id="$audioPlayer?.currentTrackId.value"
-          :aria-label="`${fetchedAlbum?.name} track`"
-          @play-item="onItemPlay"
-          @pause-item="onItemPause"
-        />
-        <AlbumTrackList
-          v-else
-          :items="fetchedAlbumTracks?.items"
-          :is-playing="$audioPlayer?.isPlaying.value"
-          :current-item-id="$audioPlayer?.currentTrackId.value"
-          @play-item="onItemPlay"
-          @pause-item="onItemPause"
-        />
-      </div>
+    <UISection heading="Top Tracks" hidden-heading content-container>
+      <MobileTrackList
+        v-if="isMobile"
+        :items="fetchedAlbumTracks?.items"
+        :is-playing="$audioPlayer?.isPlaying.value"
+        :current-item-id="$audioPlayer?.currentTrackId.value"
+        :aria-label="`${fetchedAlbum?.name} track`"
+        @play-item="onItemPlay"
+        @pause-item="onItemPause"
+      />
+      <AlbumTrackList
+        v-else
+        :items="fetchedAlbumTracks?.items"
+        :is-playing="$audioPlayer?.isPlaying.value"
+        :current-item-id="$audioPlayer?.currentTrackId.value"
+        @play-item="onItemPlay"
+        @pause-item="onItemPause"
+      />
     </UISection>
 
-    <UISection content-container>
+    <UISection heading="Album Copyright" hidden-heading content-container>
       <UIText appearance="secondary" size="12px">
-        © {{ releaseYear }} {{ mainAlbumArtistName }}. All copyrights, performance rights, and
-        related rights are owned and controlled by the artist.
+        © {{ releaseAlbumYear }} {{ mainAlbumArtist?.name }}. All copyrights, performance rights,
+        and related rights are owned and controlled by the artist.
       </UIText>
     </UISection>
 
     <UISection
-      v-if="fetchedSimilarAlbums.length"
+      :heading="`More by ${mainAlbumArtist?.name}`"
+      heading-container
+      :content-container="!isMobileOrTablet"
+    >
+      <SkeletonCardLinkListLoader v-if="isFetching" />
+      <AlbumCardLinksSlider
+        v-else-if="isMobileOrTablet"
+        :items="fetchedMainArtistAlbums"
+        show-meta
+        class="album-page__slider"
+      />
+      <AlbumCardLinksList v-else :items="fetchedMainArtistAlbums" max-rows="1" show-meta />
+    </UISection>
+
+    <UISection
       heading="You Might Also Like"
       heading-container
       :content-container="!isMobileOrTablet"
     >
+      <SkeletonCardLinkListLoader v-if="isFetching" />
       <AlbumCardLinksSlider
-        v-if="isMobileOrTablet"
+        v-else-if="isMobileOrTablet"
         :items="fetchedSimilarAlbums"
         show-artists
         class="album-page__slider"
       />
-      <AlbumCardLinksList
-        v-else
-        :items="fetchedSimilarAlbums"
-        max-rows="1"
-        show-artists
-        class="album-page__album-card-links-list"
-      />
+      <AlbumCardLinksList v-else :items="fetchedSimilarAlbums" max-rows="1" show-artists />
     </UISection>
   </div>
 </template>
@@ -60,9 +67,16 @@
 <script setup lang="ts">
 import AlbumTrackList from '~/modules/track/components/AlbumTrackList/AlbumTrackList.vue';
 import { albumApiService } from '~/modules/album/services/album.api.service';
-import type { AlbumRO, TrackRO, TracksRO } from '~/api/api.module';
-import type { ApiError } from '~/modules/shared/errors/api-error';
 import { popularApiService } from '~/modules/popular/services/popular.api.service';
+import { artistApiService } from '~/modules/artist/services/artist.api.service';
+import type {
+  AlbumRO,
+  SimplifiedAlbumRO,
+  SimplifiedArtistRO,
+  TrackRO,
+  TracksRO,
+} from '~/api/api.module';
+import type { ApiError } from '~/modules/shared/errors/api-error';
 
 const route = useRoute();
 const { $audioPlayer } = useNuxtApp();
@@ -71,8 +85,9 @@ const [isFetching, toggleFetching] = useToggle(true);
 
 const albumIdRouteParam = route.params.id as string;
 
-const { data: fetchedAlbum, error } = await useAsyncData(`album:${albumIdRouteParam}`, () =>
-  albumApiService.getAlbumById(albumIdRouteParam),
+const { data: fetchedAlbum, error } = await useAsyncData<AlbumRO>(
+  `album:${albumIdRouteParam}`,
+  () => albumApiService.getAlbumById(albumIdRouteParam),
 );
 
 if (error.value) {
@@ -81,21 +96,6 @@ if (error.value) {
     statusMessage: error.value.message,
   });
 }
-
-const fetchedAlbumTracks = ref<TracksRO | null>(null);
-const fetchedSimilarAlbums = ref<AlbumRO[]>([]);
-const mainAlbumArtistName = computed<string | null>(() => {
-  if (!fetchedAlbum.value) return null;
-
-  const [mainArtist] = fetchedAlbum.value.artists;
-
-  return mainArtist.name;
-});
-const releaseYear = computed<number | null>(() => {
-  if (!fetchedAlbum.value?.releaseAt) return null;
-
-  return new Date(fetchedAlbum.value.releaseAt).getFullYear();
-});
 
 if (fetchedAlbum.value) {
   useHead({
@@ -109,20 +109,44 @@ if (fetchedAlbum.value) {
   });
 }
 
+const fetchedAlbumTracks = ref<TracksRO | null>(null);
+const fetchedMainArtistAlbums = ref<SimplifiedAlbumRO[]>([]);
+const fetchedSimilarAlbums = ref<AlbumRO[]>([]);
+
+const mainAlbumArtist = computed<SimplifiedArtistRO | null>(() => {
+  if (!fetchedAlbum.value) return null;
+
+  const [artist] = fetchedAlbum.value.artists;
+
+  return artist;
+});
+const releaseAlbumYear = computed<number | null>(() => {
+  if (!fetchedAlbum.value?.releaseAt) return null;
+
+  return new Date(fetchedAlbum.value.releaseAt).getFullYear();
+});
+
 onMounted(async () => {
-  await fetchAlbumData(albumIdRouteParam);
+  if (fetchedAlbum.value) {
+    await fetchAlbumTracks(fetchedAlbum.value);
+    await fetchSimilarAlbums(fetchedAlbum.value);
+    await fetchArtistAlbums(fetchedAlbum.value);
+  }
+
   toggleFetching(false);
 });
 
-async function fetchAlbumData(id: string) {
+function onItemPlay(_: TrackRO, index: number) {
+  $audioPlayer?.addTracks(fetchedAlbumTracks.value?.items || [], index, { playAfterAdded: true });
+}
+
+function onItemPause() {
+  $audioPlayer?.pause();
+}
+
+async function fetchAlbumTracks({ id }: AlbumRO) {
   try {
     fetchedAlbumTracks.value = await albumApiService.getAlbumTracksById(id);
-
-    if (fetchedAlbum.value) {
-      const albumGenres = fetchedAlbum.value.genres.map(({ value }) => value);
-      const { items } = await popularApiService.getAlbums(albumGenres, { limit: 10 });
-      fetchedSimilarAlbums.value = items;
-    }
   } catch (e) {
     const { message } = e as ApiError;
 
@@ -130,27 +154,47 @@ async function fetchAlbumData(id: string) {
   }
 }
 
-function onItemPlay(item: TrackRO, index: number) {
-  $audioPlayer?.addTracks(fetchedAlbumTracks.value?.items || [], index, { playAfterAdded: true });
+async function fetchArtistAlbums({ id: albumId, artists }: AlbumRO) {
+  try {
+    const [mainArtist] = artists;
+    const { items } = await artistApiService.getAlbumsById(mainArtist.id, { limit: 10 });
+
+    // TODO: delete filter
+    fetchedMainArtistAlbums.value = items.filter(({ id }) => id !== albumId);
+  } catch (e) {
+    const { message } = e as ApiError;
+
+    console.error(message);
+  }
 }
 
-function onItemPause(item: TrackRO, index: number) {
-  $audioPlayer?.pause();
+// TODO: replace with /albums/:id/similar
+async function fetchSimilarAlbums(album: AlbumRO) {
+  try {
+    const genres = album.genres.map(({ value }) => value);
+
+    const { items } = await popularApiService.getAlbums(genres, { limit: 10 });
+
+    // TODO: delete filter
+    fetchedSimilarAlbums.value = items.filter(({ id }) => id !== album.id);
+  } catch (e) {
+    const { message } = e as ApiError;
+
+    console.error(message);
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-@use '~/assets/scss/container';
+@use '@/assets/scss/container';
 
 .album-page {
-  &__slider {
-    @extend .container;
+  @include respond-to(lg) {
+    --card-link-image-width: 140px;
   }
 
-  &__album-card-links-list {
-    @include respond-to(lg) {
-      --card-link-image-width: 140px;
-    }
+  &__slider {
+    @extend .container;
   }
 }
 </style>
