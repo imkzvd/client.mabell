ee1b32ebee347636be08bfecc211a423909be834
imkzvd <imkzvd@gmail.com>
2025-11-19 08:24:58 +0300

    feat: updated "UIButton" component
    Refs: #75

--- modules/shared/components/UI/UIButton/UIButton.vue
+++ modules/shared/components/UI/UIButton/UIButton.vue
@@ -1,57 +1,50 @@
 <template>
-  <button :type="type" :disabled="isDisabled || isLoading" :class="cssClasses">
-    <span v-if="isLoading" :class="`${baseClass}__spinner-wrapper`">
-      <UISpinner :is-contrasted="appearance === 'secondary'" />
+  <button
+    :type="type"
+    :disabled="isDisabled || isLoading"
+    class="ui-button"
+    :class="rootCSSClasses"
+  >
+    <span v-if="isLoading" class="ui-button__spinner-wrapper">
+      <UISpinner :is-contrasted="appearance === UIAppearances.primary" />
     </span>
 
-    <span :class="`${baseClass}__content`">
-      <slot name="default" />
+    <span class="ui-button__text">
+      <slot name="default">Button</slot>
     </span>
   </button>
 </template>
 
 <script setup lang="ts">
-import {
-  UIButtonAppearances,
-  type UIButtonProps,
-  UIButtonSizes,
-  UIButtonTypes,
-} from '~/modules/shared/components/UI/UIButton/types';
+import { type UIButtonProps, UIButtonTypes } from '~/modules/shared/components/UI/UIButton/types';
+import { UIAppearances, UISizes } from '~/modules/shared/components/UI/types';
 
 const props = withDefaults(defineProps<UIButtonProps>(), {
   type: UIButtonTypes.button,
-  appearance: UIButtonAppearances.primary,
-  size: UIButtonSizes.md,
+  appearance: UIAppearances.primary,
+  size: UISizes.md,
   isFull: false,
   isDisabled: false,
   isLoading: false,
 });
 
-const baseClass = 'ui-button';
-const cssClasses = computed(() => ({
-  [baseClass]: true,
-  [`${baseClass}_appearance_${props.appearance}`]: props.appearance,
-  [`${baseClass}_size_${props.size}`]: props.size,
-  [`${baseClass}_is-full`]: props.isFull,
-  [`${baseClass}_is-disabled`]: props.isDisabled,
-  [`${baseClass}_is-loading`]: props.isLoading,
+const rootCSSClass = 'ui-button';
+const rootCSSClasses = computed<Record<string, boolean>>(() => ({
+  [`${rootCSSClass}_appearance_${props.appearance}`]: !!props.appearance,
+  [`${rootCSSClass}_size_${props.size}`]: !!props.size,
+  [`${rootCSSClass}_is-full`]: props.isFull,
+  [`${rootCSSClass}_is-disabled`]: props.isDisabled,
+  [`${rootCSSClass}_is-loading`]: props.isLoading,
 }));
 </script>
 
 <style lang="scss" scoped>
-$base-class: ui-button;
-
-.#{$base-class} {
-  font-family: CircularSpTitleBold, sans-serif;
+.ui-button {
   position: relative;
   border: none;
   border-radius: var(--ui-button-rounded, 4px);
   cursor: pointer;
-  padding-block: 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  transition: all 0.25s ease;
 
   &:deep(svg) {
     fill: currentColor;
@@ -59,26 +52,26 @@ $base-class: ui-button;
 
   &_appearance {
     &_primary {
-      background-color: var(--ui-button-bg-color, #ffffff);
-      color: var(--ui-button-text-color, #000000);
+      background-color: var(--ui-button-bg-color, white);
+      color: var(--ui-button-text-color, black);
 
       @include respond-to(xl) {
-        &:not(.#{$base-class}_loading, :disabled) {
+        &:not(.ui-button_loading, :disabled) {
           &:hover {
-            background-color: var(--ui-button-hover-bg-color, #f4f4f4ff);
+            background-color: var(--ui-button-hover-bg-color, gray);
           }
         }
       }
     }
 
     &_secondary {
-      background-color: var(--ui-button-secondary-bg-color, #ffffff);
-      color: var(--ui-button-secondary-text-color, #000000);
+      background-color: var(--ui-button-secondary-bg-color, gray);
+      color: var(--ui-button-secondary-text-color, white);
 
       @include respond-to(xl) {
-        &:not(.#{$base-class}_loading, :disabled) {
+        &:not(.ui-button_loading, :disabled) {
           &:hover {
-            background-color: var(--ui-button-secondary-hover-bg-color, #f4f4f4ff);
+            background-color: var(--ui-button-secondary-hover-bg-color, gray);
           }
         }
       }
@@ -109,23 +102,16 @@ $base-class: ui-button;
     width: 100%;
   }
 
-  &_is-loading {
-    .#{$base-class}__content {
+  &_is-loading & {
+    &__text {
       opacity: 0;
     }
   }
 
   &_is-disabled {
     cursor: not-allowed;
-    background-color: var(--ui-button-disabled-bg-color, #c6c6c6);
-    color: var(--ui-button-disabled-text-color, #222222);
-  }
-
-  &__content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 4px;
+    background-color: var(--ui-button-disabled-bg-color, gray);
+    color: var(--ui-button-disabled-text-color, darkgray);
   }
 
   &__spinner-wrapper {
